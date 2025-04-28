8b96520ed9fc1333cdd9eb6b67c75668f14606ac
LakshS2710 <lakshshadija@gmail.com>
2025-04-28 15:22:07 +0530

    First Question

--- Patern Problems/q1.cpp
+++ Patern Problems/q1.cpp
@@ -1,7 +1,22 @@
+// Q-1 square pattern 1234 
+//                    1234
+//                    1234
+//                    1234
+// For n = 4
+
+
 #include <iostream>
 using namespace std;
 
 int main () {
-    cout << "Hello World";
+    int n = 4;
+    for(int i=0; i <=n-1; i++){
+        for( int j=1; j<=n ; j++){
+            cout << j << "";
+        }
+        cout << endl;
+    }
+    
+
     return 0;
 }
