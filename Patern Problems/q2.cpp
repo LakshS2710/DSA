76874e6d19571d106f508d67a14835a2504ba5e8
LakshS2710 <lakshshadija@gmail.com>
2025-04-30 14:40:28 +0530

    question 3

--- Patern Problems/q2.cpp
+++ Patern Problems/q2.cpp
@@ -7,15 +7,15 @@
  using namespace std;
 
  int main(){
-    int n = 4;
-    for(int i=0 ; i <= n-1 ; i++){
+    int n = 5;
+    for(int i=0; i<n ; i++){
         char ch = 'A';
-        for(int j=1; j<=n; j++){
-            cout << ch << " ";
-            ch = ch+1;
+        for(int j=0; j<n; j++){
+            cout << ch;
+            ch = ch + 1;
         }
         cout << endl;
     }
-
+    
     return 0;
  }
